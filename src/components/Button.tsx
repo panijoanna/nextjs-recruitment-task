c75674fca635c61b37ab5532ecfdfbb66ca2007a
panijoanna <pani.joanna93@gmail.com>
2025-02-24 13:46:25 +0100

    feat: create a reusable button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  text: string;
+  variant: "editButton" | "deleteButton" | "createButton";
+}
+
+const Button = ({ text, variant }: ButtonProps) => {
+  const buttonStyles = {
+    editButton:
+      "px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-600 transition-all",
+    deleteButton:
+      "px-4 py-2 border border-red-600 text-red-600 rounded-lg hover:bg-red-600 hover:text-white transition-all",
+    createButton:
+      "px-6 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-all",
+  };
+
+  return <button className={`${buttonStyles[variant]}`}>{text}</button>;
+};
+
+export default Button;
